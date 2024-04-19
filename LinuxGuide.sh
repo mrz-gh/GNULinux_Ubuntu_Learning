f0d9e112d7d0dd253d522d7fa9ff262e861ed905
AmirrezaGh <mrz-gh01@gmail.com>
2024-04-19 19:23:33 +0330

    The first commands when you interact with a new Linux for the first time

--- LinuxGuide.sh
+++ LinuxGuide.sh
@@ -1,7 +1,23 @@
+# The first commands when you interact with a new Linux for the first time
+uptime
+id
+lshw
+lspci -k # shows the drivers
+lsb_release -d
+
+
 # This is a guide to linux commands
 touch script.sh	# make a file
 chmod +x script.sh # make it executable
 nano script.sh # in order to write in the script file
 ./script.sh # execute an executable file
 which chmod # shows the location of the command
-lspci -k # shows the drivers
+su <user> # change to a user
+ls -altrh
+
+#show the content of a file
+cat
+bat
+# Editing a file 
+nano
+vi # vim
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 Last Update: 2024-4-19
 # What
 This is a repository for GNU/Linux self-learning
+
+
+
+# My Useful Plugins
+-   fzf terminal plugin
+-   bat package
+-   zsh, oh-my-zsh, **zsh-syntax-highlighting**
